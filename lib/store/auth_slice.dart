d8b0a42198b724b34846a344c43e5fb3eecfe3bc
cezarmocanu <cezarmocanu2014@gmail.com>
2023-04-16 01:31:50 +0300

    [device-id] Saved token to local storage

--- lib/store/auth_slice.dart
+++ lib/store/auth_slice.dart
@@ -2,48 +2,51 @@ import 'dart:convert';
 import 'dart:io';
 
 import 'package:ai_drink/models/auth/auth_identity_response_dto/auth_identity_response_dto.dart';
-import 'package:ai_drink/models/drink/drink.dart';
 import 'package:ai_drink/models/user_identity/user_identity_get_dto/user_identity_get_dto.dart';
 import 'package:ai_drink/services/auth_service.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart';
 import 'package:platform_device_id/platform_device_id.dart';
-
-import '../services/drink_service.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class AuthSlice extends ChangeNotifier {
   AuthService _authService = AuthService();
 
-  String? _deviceId;
+  late SharedPreferences _preferences;
   late UserIdentityGetDto _userIdentity;
 
   UserIdentityGetDto get userIdentity => _userIdentity;
 
-  AuthSlice() {
-    loadDeviceId();
-  }
+  AuthSlice() {}
 
-  void loadDeviceId() async {
-    _deviceId = await PlatformDeviceId.getDeviceId;
-    postIdentity();
-    // TODO handle case in which deviceId is null
+  void init() async {
+    String? _deviceId = await PlatformDeviceId.getDeviceId;
+    final authIdentity = await postIdentity(_deviceId);
 
-    notifyListeners();
-  }
-
-  void postIdentity() async {
-    if (_deviceId == null) {
+    if (authIdentity == null) {
       return;
     }
 
-    Response r = await _authService.postIdentity(_deviceId!);
+    _userIdentity = authIdentity.identity;
+    _preferences = await SharedPreferences.getInstance();
+    //TODO add constant to key
+    await _preferences.setString('token', authIdentity.token);
+    print(authIdentity.token);
+    notifyListeners();
+  }
 
-    if ([HttpStatus.ok, HttpStatus.created].contains(r.statusCode)) {
-      _userIdentity = AuthIdentityResponseDto.fromJson(json.decode(r.body)).identity;
-    } else {
-      print(String.fromCharCodes(r.bodyBytes));
+  Future<AuthIdentityResponseDto?> postIdentity(String? deviceId) async {
+    if (deviceId == null) {
+      return null;
     }
 
-    notifyListeners();
+    Response r = await _authService.postIdentity(deviceId);
+
+    if ([HttpStatus.ok, HttpStatus.created].contains(r.statusCode)) {
+      return AuthIdentityResponseDto.fromJson(json.decode(r.body));
+    } else {
+      print("[AuthSlice][Error]${String.fromCharCodes(r.bodyBytes)}");
+      return null;
+    }
   }
 }
